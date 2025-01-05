eb2563553e2fc1fc90beccdc27d83b856330937b
sanabel-al-firdaws <abdullhakim.sami@gmail.com>
2025-01-05 11:02:16 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <div class="element"></div>
 `
 
-import { Editor } from '@tiptap/core'
+import { Editor, Extension } from '@tiptap/core'
 import StarterKit from '@tiptap/starter-kit'
 
 import {
@@ -13,43 +13,65 @@ import {
   LoroCursorPlugin,
   LoroSyncPlugin,
   LoroUndoPlugin,
-  undo,
-  redo,
 } from "loro-prosemirror";
 import { LoroDoc } from "loro-crdt";
-import { keymap } from '@tiptap/pm/keymap'
 
 
 const doc = new LoroDoc();
 const awareness = new CursorAwareness(doc.peerIdStr);
 
-// this example loads the EditorState class from the ProseMirror state package
-import { EditorState } from '@tiptap/pm/state'
 
 
+let loroSyncPlugin = Extension.create({
+  name: 'LoroSyncPlugin',
 
+  addProseMirrorPlugins() {
+    return [
+      LoroSyncPlugin({ doc })
+    ]
+  },
+})
+let loroCursorPlugin = Extension.create({
+  name: 'LoroCursorPlugin',
+
+  addProseMirrorPlugins() {
+    return [
+      LoroCursorPlugin(awareness, {}),
+    ]
+  },
+})
+
+let loroUndoPlugin = Extension.create({
+  name: 'LoroUndoPlugin',
+
+  addProseMirrorPlugins() {
+    return [
+      LoroUndoPlugin({
+        doc: doc
+      }),]
+  },
+})
 
 
 const editor = new Editor({
 
   element: document.querySelector('.element')!,
-  extensions: [StarterKit.configure({ history: false })],
+  extensions: [StarterKit.configure({ history: false }), loroSyncPlugin, loroCursorPlugin, loroUndoPlugin],
   content: '<p>Hello World!</p>',
 })
-const plugins = [
-  LoroSyncPlugin({
-    doc: doc
-  }),
-  LoroUndoPlugin({
-    doc: doc
-  }),
-  keymap({
-    "Mod-z": undo,
-    "Mod-y": redo,
-    "Mod-Shift-z": redo,
-  }),
-  LoroCursorPlugin(awareness, {}),
-];
+// const plugins = [
+//   LoroSyncPlugin({
+//     doc: doc
+//   }),
+//   LoroUndoPlugin({
+//     doc: doc
+//   }),
+//   keymap({
+//     "Mod-z": undo,
+//     "Mod-y": redo,
+//     "Mod-Shift-z": redo,
+//   }),
+// ];
 
 
-editor.state = EditorState.create({ doc, plugins })
+// editor.state = EditorState.create({ doc, plugins })
